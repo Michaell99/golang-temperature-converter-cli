7ee46aeaab7a02881024dc070cfd58b2d71283f0
Michael <Michael1@Michaels-MacBook-Pro.local>
2023-04-04 20:12:17 +0300

    0.0.2

--- main.go
+++ main.go
@@ -25,9 +25,8 @@ func main() {
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-		fmt.Scanln(&originValue)
-		var _ = &originValue
-		var err = &originValue
+		_, err = fmt.Scanln(&originValue)
+
 		if err != nil {
 			printError(errReadingInput)
 		}
@@ -37,15 +36,12 @@ func main() {
 			convertToCelsius(originValue)
 		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
-		fmt.Scanln(&shouldConvertAgain)
-		var _ = &shouldConvertAgain
-		var err = &shouldConvertAgain
+		_, err = fmt.Scanln(&shouldConvertAgain)
+
 		if err != nil {
 			printError(errReadingInput)
 		}
-		strings.TrimSpace(shouldConvertAgain)
-		strings.ToUpper(shouldConvertAgain)
-		if shouldConvertAgain != "Y" {
+		if strings.TrimSpace(strings.ToUpper(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
